a4e872b91cea6c0dfd524196eee325d2ce6971f7
Aladin Júnior <aladinjr013@icloud.com>
2023-08-20 12:23:38 -0300

    nav controller used

--- app/src/main/java/co/aladinjunior/joker/MainActivity.kt
+++ app/src/main/java/co/aladinjunior/joker/MainActivity.kt
@@ -1,11 +1,33 @@
 package co.aladinjunior.joker
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment_container)
+        navigationView.setupWithNavController(navController)
+
+
+
     }
 }
